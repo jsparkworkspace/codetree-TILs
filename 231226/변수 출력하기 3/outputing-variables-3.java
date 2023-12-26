7235dc17603a7c7dfb501068c6308ab941874772
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2023-12-26 09:10:00 +0900

    Title: 변수 출력하기 3, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231226/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a, b;
+        char c = 'C';
+        a = 1;
+        b = 2;
+        System.out.printf("%d->%d->%c", a, b, c);
+    }
+}
