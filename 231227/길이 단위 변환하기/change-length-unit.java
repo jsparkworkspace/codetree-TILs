2a01564ad117cfa71b9eac154b09770a9134a64a
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2023-12-27 09:14:15 +0900

    Title: 길이 단위 변환하기, Time: 94ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 231227/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        float a = 30.48f;
+        float b = 160934f;
+        System.out.printf("9.2ft = %.1fcm\n",(a*9) + (a/10*2));
+        System.out.printf("1.3mi = %.1fcm",b + (b/10*3));
+    }
+}
