018a34ef36944fc6ba9726932457eac66d17b06c
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-01-08 09:17:09 +0900

    Title: 합과 차의 나눗셈, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240108/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        float c = a + b;
+        float d = a - b;
+        System.out.printf("%.2f", c / d);
+    }
+}
