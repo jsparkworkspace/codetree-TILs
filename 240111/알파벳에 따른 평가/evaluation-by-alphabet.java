c8a489f3bed248c0c077a82ec63d38dcad24c3f1
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-01-11 17:33:48 +0900

    Title: 알파벳에 따른 평가, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240111/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char c = str.charAt(0);
+
+        if (c == 'S') {
+            System.out.print("Superior");
+        } else if (c == 'A') {
+            System.out.print("Excellent");
+        } else if (c == 'B') {
+            System.out.print("Good");
+        } else if (c == 'C') {
+            System.out.print("Usually");
+        } else if (c == 'D') {
+            System.out.print("Effort");
+        } else {
+            System.out.print("Failure");
+        }
+        sc.close();
+    }
+}
