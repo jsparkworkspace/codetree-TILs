3189a4247b1062160e2e08c7c57da13544eed24a
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-01-13 21:02:10 +0900

    Title: 윤년인가, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240113/윤년인가/is-leap-year.java
+++ 240113/윤년인가/is-leap-year.java
@@ -5,11 +5,18 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int y = sc.nextInt();
 
-        if ((y % 4 == 0) || ((y % 4 == 0) && (y % 100 == 0))) {
+        
+
+        if ( (y % 4 == 0) && (y % 100 == 0) && (y % 400 == 0)) {
+            System.out.print("true");
+        } else if ( (y % 4 == 0) && (y % 100 == 0)) {
+            System.out.print("false");
+        } else if (y % 4 == 0) {
             System.out.print("true");
         } else {
-            System.out.print(false);
+            System.out.print("false");
         }
+              
         sc.close();
     }
 }
