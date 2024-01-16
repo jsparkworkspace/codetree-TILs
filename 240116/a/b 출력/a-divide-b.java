be4c286b0aa1a60a7f029236f59b9ac3056c94c8
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-01-16 09:17:14 +0900

    Title: a/b 출력, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240116/a/b 출력/a-divide-b.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+       Scanner sc = new Scanner(System.in);
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    int value,remain;
+
+    value = a/b;
+    remain = a%b;
+
+    System.out.print(value);
+    System.out.print(".");
+
+    for (int i = 0; i < 20; i ++) {
+
+        remain *= 10;
+        value = remain/b;
+        remain = remain%b;
+        System.out.print(value);
+
+    }
+ }
+}
