8ddab77a6526073a342590360a00018755b1fc87
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-01-16 14:01:36 +0900

    Title: 숫자의 증감, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

--- 240116/숫자의 증감/increasing-and-decreasing-numbers.java
+++ 240116/숫자의 증감/increasing-and-decreasing-numbers.java
@@ -3,21 +3,21 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        String c = sc.next();
-        int n = sc.nextInt();
-        char s = c.charAt(0);
-        if (s == 'A') {
-            for (int i = 1; i <= n; i++) {
-                System.out.print(i + " ");
-            }
-
-        if (s == 'D') {
-            for (int i = n; i > 0; i--) {
-                System.out.print(i + " ");
-            }
-        }
-
-        sc.close();
-        }
+    String c = sc.next();
+    int n = sc.nextInt();
+    char s = c.charAt(0);
+    if (s == 'A') {
+      for (int i = 1; i <= n; i++) {
+        System.out.print(i + " ");
+      }
     }
+    if (s == 'D') {
+      for (int i = n; i > 0; i--) {
+        System.out.print(i + " ");
+      }
+    }
+
+    sc.close();
+
+  }
 }
