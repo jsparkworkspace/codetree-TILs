92b8807fa8de596cbf9fa7c60b54e38fe1b40569
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-01-16 19:55:54 +0900

    Title: 청소당번 정하기, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240116/청소당번 정하기/cleaning-numbering.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        
+        for (int i = 1; i <=n; i++) {
+        if (i % 12 == 0) {
+            c++;
+        } else if (i % 3 == 0) {
+            b++;
+        } else if (i % 2 == 0) {
+            a++;
+        } else {
+
+        }
+        }
+        System.out.printf("%d %d %d", a, b, c);
+        sc.close();
+}
+}
