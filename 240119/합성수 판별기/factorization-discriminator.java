ec130fcabb73e97a1fb8618b270ed23cb16bfb76
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-01-19 09:29:14 +0900

    Title: 합성수 판별기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240119/합성수 판별기/factorization-discriminator.java
+++ 240119/합성수 판별기/factorization-discriminator.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         boolean satisfied = false;
-        for (int i = 2; i <= n; i++) {
+        for (int i = 2; i < n; i++) {
             if (n % i == 0) {
                 satisfied = true;
             }
