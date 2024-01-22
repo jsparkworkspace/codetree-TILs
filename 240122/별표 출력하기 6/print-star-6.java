3b12c2f5483c0d8512f2b7e0e48033c8364be073
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-01-22 11:35:36 +0900

    Title: 별표 출력하기 6, Time: 190ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240122/별표 출력하기 6/print-star-6.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < 2 * i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2 * n - 1 - (2 * i); j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+
+            for (int j = 0; j < (2 * n - 4) - (2 * i); j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 3 + 2 * i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
