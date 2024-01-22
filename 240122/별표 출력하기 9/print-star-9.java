9e1f104cb1e8b409ef7c9231f50bf07548b33d4d
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-01-22 10:46:09 +0900

    Title: 별표 출력하기 9, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240122/별표 출력하기 9/print-star-9.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            
+            for (int j = 2 * n - 2 * i; j > 0; j--) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
