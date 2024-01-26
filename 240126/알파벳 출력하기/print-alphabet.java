8d9eb91fd1a7d98eb0c6700b1d897ef4a3656ea9
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-01-26 10:08:04 +0900

    Title: 알파벳 출력하기, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240126/알파벳 출력하기/print-alphabet.java
+++ 240126/알파벳 출력하기/print-alphabet.java
@@ -9,7 +9,7 @@ public class Main {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i + 1; j++) {
                 System.out.print(c++);
-                if (c == 'Z') c = 'A';
+                if (c > 'Z') c = 'A';
             }
             System.out.println();
         }
