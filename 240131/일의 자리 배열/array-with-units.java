b717ff3c2f113aa68ce94b01b0a9cc9f05c7a5cf
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-01-31 19:25:29 +0900

    Title: 일의 자리 배열, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240131/일의 자리 배열/array-with-units.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] num = br.readLine().split(" ");
+        int[] result = new int[10];
+        result[0] = Integer.parseInt(num[0]);
+        result[1] = Integer.parseInt(num[1]);
+
+        for (int i = 0; i < result.length; i++) {
+            if (i >= 2) result[i] = result[i-2] + result[i-1];
+            System.out.print(result[i] % 10 + " ");
+        }
+
+        br.close();
+        bw.close();
+    }
+}
