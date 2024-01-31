eba70a73197dffc6b7bffed5a1f7892e180cd848
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-01-31 18:51:06 +0900

    Title: 제곱하여 출력하기, Time: 114ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240131/제곱하여 출력하기/print-square-of-elements.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = br.readLine().split(" ");
+
+        for (int i = 0; i < n; i++) {
+            bw.write(Integer.parseInt(arr[i]) * Integer.parseInt(arr[i]) + " ");
+        }
+        
+        br.close();
+        bw.close();
+    }
+}
