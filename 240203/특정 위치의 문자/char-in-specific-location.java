24af442a68cb2c54850d0d085658327687f91d4c
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-03 10:58:55 +0900

    Title: 특정 위치의 문자, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240203/특정 위치의 문자/char-in-specific-location.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        char[] arr = {'L', 'E', 'B', 'R', 'O', 'S'};
+        
+        char c = sc.next().charAt(0);
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == c) {
+                System.out.print(i);
+            }
+        }
+
+        sc.close();
+    }
+}
