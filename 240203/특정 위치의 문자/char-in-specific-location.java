78080c894cba282355c9f8f7cb3730f95fced667
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-03 11:19:26 +0900

    Title: 특정 위치의 문자, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

--- 240203/특정 위치의 문자/char-in-specific-location.java
+++ 240203/특정 위치의 문자/char-in-specific-location.java
@@ -1,20 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         char[] arr = {'L', 'E', 'B', 'R', 'O', 'S'};
+
+        char c = br.readLine().charAt(0);
+        
         boolean b = false;
-        char c = sc.next().charAt(0);
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == c) {
                 b = true;
-                System.out.print(i);
+                bw.write(i + "");
             }
         }
-        if (b == false) System.out.print("None");
+        if (b == false) bw.write("None");
 
-        sc.close();
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
