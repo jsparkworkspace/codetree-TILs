abdeadd7873e863c5e169f4e029b80be4a1affe2
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-03 10:24:22 +0900

    Title: 나눗셈의 나머지, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240203/나눗셈의 나머지/remainder-of-division.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int[] arr = new int[10];
+        int sum = 0;
+        for (;;) {
+            arr[a % b]++;
+            a /= b;
+            if (a <= 1) break;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i] * arr[i];
+        }
+        System.out.print(sum);
+
+        sc.close();
+    }
+}
