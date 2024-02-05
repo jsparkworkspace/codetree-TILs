538bae533d05f41ab201b0d85ca9815d245d27be
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-05 09:27:15 +0900

    Title: 999 또는 -999, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240205/999 또는 -999/999-or-999.java
+++ 240205/999 또는 -999/999-or-999.java
@@ -4,10 +4,11 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int maxNo = 0;
-        int minNo = 0;
+        int n = sc.nextInt();
+        int maxNo = n;
+        int minNo = n;
         for (;;) {
-            int n = sc.nextInt();
+            n = sc.nextInt();
             if (n == 999 || n == -999) {
                 break;
             }
