4df43556175e61d12574a3c30ca31c2f0354cd4c
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-05 09:47:31 +0900

    Title: n개의 숫자 중 최대 2개, Time: 129ms, Memory: 9MB, Status: Passed - CodeTree

--- 240205/n개의 숫자 중 최대 2개/two-max-of-n-num.java
+++ 240205/n개의 숫자 중 최대 2개/two-max-of-n-num.java
@@ -1,29 +1,32 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))){
 
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        for (int i = 0; i < n; i++) {
+            int n = Integer.parseInt(br.readLine());
+            String[] arr = br.readLine().split(" ");
             
-            for (int j = i; j < n; j++) {
-                if (arr[i] < arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+            for (int i = 0; i < arr.length; i++) {
+
+                for (int j = i; j < arr.length; j++) {
+                    if (Integer.parseInt(arr[i]) < Integer.parseInt(arr[j])) {
+                        String temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
                 }
+
             }
+            bw.write(arr[0] + " " + arr[1]);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        System.out.printf("%d %d", arr[0], arr[1]);
-
-        sc.close();
     }
 }
