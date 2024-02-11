915bef50f7032a32618c2b46b03f1556dfbac48c
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-11 22:55:47 +0900

    Title: 가장 짧은 문자열, Time: 143ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240211/가장 짧은 문자열/shortest-string.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String[] str = new String[3];
+        int max = 0;
+        int min = 0;
+        for (int i = 0; i < str.length; i++) {
+            str[i] = sc.next();
+            if (i == 0) {
+                max = str[i].length();
+                min = str[i].length();
+            } else {
+                if (str[i-1].length() < str[i].length()) {
+                    max = str[i].length();
+                } else {
+                    min = str[i].length();
+                }
+            }
+        }
+
+        System.out.print(max - min);
+
+        sc.close();
+    }
+}
