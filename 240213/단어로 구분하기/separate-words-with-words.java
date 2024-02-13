64ad68e061d444d5e55a9af6f9952fc2e4d2f080
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-13 09:35:34 +0900

    Title: 단어로 구분하기, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240213/단어로 구분하기/separate-words-with-words.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String[] arr = new String[10];
+
+        String str = sc.nextLine();
+        arr = str.split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+        sc.close();
+    }
+}
