a3757abf38ebc924d6298df5f27fd11b54db8f8f
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-14 14:15:39 +0900

    Title: 특정 문자의 유무, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240214/특정 문자의 유무/specific-character-presence.java
+++ 240214/특정 문자의 유무/specific-character-presence.java
@@ -1,23 +1,22 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-
         String str = sc.next();
-        String b1 = "No";
-        String b2 = "No";
-        for (int i = 0; i < str.length() - 2; i++) {
-            if (str.substring(i, i+2).equals("ee")) {
-                b1 = "Yes";
-            } else if (str.substring(i, i+2).equals("ab")) {
-                b2 = "Yes";
-            } else {
-                continue;
-            }
+
+        if(str.contains("ee")) {
+            System.out.print("Yes ");
+        }
+        else {
+            System.out.print("No ");
         }
 
-        System.out.print(b1 + " " + b2);
-        sc.close();
+        if(str.contains("ab")) {
+            System.out.print("Yes");
+        }
+        else {
+            System.out.print("No");
+        }
     }
 }
