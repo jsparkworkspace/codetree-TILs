564c1b4288f0e1d0274e22456325767dd71969bf
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-14 21:22:03 +0900

    Title: 특정 문자의 등장 횟수, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240214/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.substring(i, i+2).equals("ee")) {
+                cnt1++;
+            }
+            if (str.substring(i,i+2).equals("eb")) {
+                cnt2++;
+            }
+        }
+
+        System.out.printf("%d %d", cnt1, cnt2);
+
+        sc.close();
+    }
+}
