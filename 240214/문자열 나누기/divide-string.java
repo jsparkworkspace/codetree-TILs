996a4756bca96902db97e05c61b6409bb256ea1e
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-14 10:02:46 +0900

    Title: 문자열 나누기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240214/문자열 나누기/divide-string.java
+++ 240214/문자열 나누기/divide-string.java
@@ -9,21 +9,15 @@ public class Main {
         for (int i = 0; i < n; i++) {
             str += sc.next();
         }
-        String[] arr = new String[str.length() / 5];
         int cnt = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = ""; // null 값을 빈 값으로 초기화
-
-            for (int j = cnt; j < str.length(); j++) { // 직전 반복 횟수를 받아 해당 문자 위치부터 반복 시작
-            arr[i] += str.charAt(j); // String + char = String
+        for (int i = 0; i < str.length(); i++) {
             cnt++;
-            if (cnt % 5 == 0) break; // 5회 반복 시 for문 종료
+            if (cnt % 5 == 0 && i != 0) {
+                System.out.println(str.charAt(i));
+            } else {
+                System.out.print(str.charAt(i));
             }
-            System.out.println(arr[i]);
         }
-
-        System.out.print(arr.length);
 
         sc.close();
     }
