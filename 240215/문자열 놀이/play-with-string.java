427033cf9b01261fbe47dac45b0c2e407c374c0a
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-15 18:53:46 +0900

    Title: 문자열 놀이, Time: 159ms, Memory: 11MB, Status: Passed - CodeTree

--- 240215/문자열 놀이/play-with-string.java
+++ 240215/문자열 놀이/play-with-string.java
@@ -6,28 +6,27 @@ public class Main {
 
         String str = sc.next();
         int q = sc.nextInt();
-        char[] arr;
 
         for (int i = 0; i < q; i++) {
             int qNum = sc.nextInt();
-            arr = str.toCharArray();
 
             if (qNum == 1) {
                 int a = sc.nextInt();
                 int b = sc.nextInt();
-                arr[a-1] = str.charAt(b-1);
-                arr[b-1] = str.charAt(a-1);
+
+                char tmp = str.charAt(a - 1);
+                str = str.substring(0, a - 1) + str.charAt(b - 1) + str.substring(a);
+                str = str.substring(0, b - 1) + tmp + str.substring(b);
                 
             } else if (qNum == 2) {
                 char a = sc.next().charAt(0);
                 char b = sc.next().charAt(0);
-                for (int j = 0; j < arr.length; j++) {
-                    if (arr[j] == a) {
-                        arr[j] = b;
+                for (int j = 0; j < str.length(); j++) {
+                    if (str.charAt(j) == a) {
+                        str = str.substring(0, j) + b + str.substring(j + 1);
                     }
                 }
             }
-            str = String.valueOf(arr);
             System.out.println(str);
         }
 
