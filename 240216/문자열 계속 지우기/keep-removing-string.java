b7824e65b72c3138e3629b25a0fc935878c70e37
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-16 13:52:54 +0900

    Title: 문자열 계속 지우기, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240216/문자열 계속 지우기/keep-removing-string.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String strA = sc.next();
+        String strB = sc.next();
+
+        for (int i = 0; i < strA.length() -strB.length(); i++) {
+
+            int n = strA.indexOf(strB);
+            if (n == -1) {
+                break;
+            }
+            else if (n == 0) {
+                strA = strA.substring(strB.length());
+            } else { 
+                strA = strA.substring(0, n) + strA.substring(n + strB.length());
+            }
+        }
+
+        System.out.print(strA);
+
+        sc.close();
+    }
+}
