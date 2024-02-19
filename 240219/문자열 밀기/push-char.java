6690faad3f00901d3e2db439bb0ac7d3cf63352d
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-19 09:25:37 +0900

    Title: 문자열 밀기, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240219/문자열 밀기/push-char.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        str = str.substring(1,str.length()) + str.substring(0,1);
+
+        System.out.print(str);
+
+        sc.close();
+    }
+}
