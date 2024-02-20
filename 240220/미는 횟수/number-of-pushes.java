26a16c82a23c1454f2643ee0bcff61e56ac62a18
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-20 15:57:55 +0900

    Title: 미는 횟수, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240220/미는 횟수/number-of-pushes.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.next();
+        String str2 = sc.next();
+        int cnt = 0;
+
+        while (!str1.equals(str2)) {
+            str1 = str1.substring(1, str1.length()) + str1.substring(0,1);
+            cnt++;
+            if (cnt >= str1.length()) {
+                cnt = -1;
+                break;
+            }
+        }
+        
+        System.out.print(cnt);
+
+        sc.close();
+    }
+}
