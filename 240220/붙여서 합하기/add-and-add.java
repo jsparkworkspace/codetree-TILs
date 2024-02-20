6f02a62d4770945c75d1c593199c08f9ba6bd57b
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-02-20 09:29:22 +0900

    Title: 붙여서 합하기, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240220/붙여서 합하기/add-and-add.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.next();
+        String str2 = sc.next();
+
+        System.out.print(Integer.parseInt(str1 + str2) + Integer.parseInt(str2 + str1));
+
+        sc.close();
+    }
+}
