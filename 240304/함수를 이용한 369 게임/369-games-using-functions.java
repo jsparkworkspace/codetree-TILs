0a012d337bf58d861b5949679ed2af0ee6c4a2a5
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-03-04 20:49:39 +0900

    Title: 함수를 이용한 369 게임, Time: 378ms, Memory: 23MB, Status: Passed - CodeTree

---
+++ 240304/함수를 이용한 369 게임/369-games-using-functions.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int game(int a, int b) {
+        int cnt = 0;
+        
+        for (int i = a; i <= b; i++) {
+            if (i % 3 == 0) {
+                cnt++;
+            } else if (String.valueOf(i).contains("3") || String.valueOf(i).contains("6") || String.valueOf(i).contains("9")) { 
+                    cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.print(game(a, b));
+        sc.close();
+    }
+}
