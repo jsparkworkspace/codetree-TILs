9009453248acbe3f6938a321a0c1404e9fc72e08
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-03-06 20:30:42 +0900

    Title: 함수를 이용한 온전수 판별, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240306/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int compute(int a, int b) {
+        int cnt = 0;
+        for (int i = a; i <= b; i++) {
+            if (i % 2 == 0) {
+                continue;
+            } else if (i % 10 == 5) {
+                continue;
+            } else if (i % 3 == 0 && i % 9 != 0) {
+                continue;
+            } else {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.print(compute(a, b));
+
+        sc.close();
+    }
+}
