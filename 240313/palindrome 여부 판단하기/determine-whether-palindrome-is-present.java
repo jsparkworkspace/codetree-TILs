769d5a7a98f70ed1c2a38df394d0d8af64e05238
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-03-13 10:09:11 +0900

    Title: palindrome 여부 판단하기, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240313/palindrome 여부 판단하기/determine-whether-palindrome-is-present.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static String isPalindrome(String str) {
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) == str.charAt(str.length() - (1 + i))) {
+                continue;
+            } else {
+                return "No";
+            }
+        }
+        return "Yes";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        System.out.print(isPalindrome(str));
+
+        sc.close();
+    }
+}
