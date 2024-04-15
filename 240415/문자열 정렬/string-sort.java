35228d5d2407e6e0978bb3a641920bc01b1234f6
jsparkworkspace <150869047+jsparkworkspace@users.noreply.github.com>
2024-04-15 13:20:11 +0900

    Title: 문자열 정렬, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240415/문자열 정렬/string-sort.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        char[] arr = str.toCharArray();
+        Arrays.sort(arr);
+
+        String sortStr = new String(arr);
+
+        System.out.println(sortStr);
+        sc.close();
+    }
+}
