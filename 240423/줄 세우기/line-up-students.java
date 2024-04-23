5e79a49947486abca8a29fb0c69cee1505bf8416
JunTR <150869047+jsparkworkspace@users.noreply.github.com>
2024-04-23 19:22:55 +0900

    Title: 줄 세우기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240423/줄 세우기/line-up-students.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Student implements Comparable<Student> {
+    int height;
+    int weight;
+    int num;
+
+    public Student(int height, int weight, int num) {
+        this.height = height;
+        this.weight = weight;
+        this.num = num;
+    }
+
+    @Override
+    public int compareTo(Student student) {
+        if (student.height != this.height) return student.height - this.height;
+        if (student.weight != this.weight) return student.weight - this.weight;
+        return student.num - this.num;
+
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Student[] students = new Student[n];
+
+        for (int i = 0; i < n; i++) {
+            int height = sc.nextInt();
+            int weight = sc.nextInt();
+            students[i] = new Student(height, weight, i + 1);
+        }
+
+        Arrays.sort(students);
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%d %d %d\n", students[i].height, students[i].weight, students[i].num);
+        }
+
+        sc.close();
+    }
+}
