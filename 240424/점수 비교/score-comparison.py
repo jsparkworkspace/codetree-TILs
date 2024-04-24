e4201b4eee0367fa311a5b00a451cebbf89783f3
JunTR <150869047+jsparkworkspace@users.noreply.github.com>
2024-04-24 14:30:12 +0900

    Title: 점수 비교, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240424/점수 비교/score-comparison.py
@@ -0,0 +1,11 @@
+arr1 = input().split()
+arr2 = input().split()
+math1 = int(arr1[0])
+eng1 = int(arr1[1])
+math2 = int(arr2[0])
+eng2 = int(arr2[1])
+
+if math1 > math2 and eng1 > eng2:
+    print(1)
+else:
+    print(0)
