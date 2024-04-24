0cbbe91eb31fac43ecfc6c1137e52a57f69fb13e
JunTR <150869047+jsparkworkspace@users.noreply.github.com>
2024-04-24 13:17:05 +0900

    Title: 알파벳에 따른 평가, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240424/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+text = input()
+
+if text == 'S':
+    print("Superior")
+elif text == 'A':
+    print("Excellent")
+elif text == 'B':
+    print("Good")
+elif text == 'C':
+    print("Usually")
+elif text == 'D':
+    print("Effort")
+else:
+    print("Failure")
