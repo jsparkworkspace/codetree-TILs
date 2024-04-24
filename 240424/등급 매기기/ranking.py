deca925e27a2b18968a1235d651b5af18c0a92bb
JunTR <150869047+jsparkworkspace@users.noreply.github.com>
2024-04-24 13:10:47 +0900

    Title: 등급 매기기, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240424/등급 매기기/ranking.py
@@ -0,0 +1,12 @@
+price = int(input())
+
+if price >= 90:
+    print("A")
+elif price >= 80:
+    print("B")
+elif price >= 70:
+    print("C")
+elif price >= 60:
+    print("D")
+else:
+    print("F")
