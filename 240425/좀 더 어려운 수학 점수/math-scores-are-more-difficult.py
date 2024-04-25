d344b26ec1cf95381c074e5cdc715027e3645185
JunTR <150869047+jsparkworkspace@users.noreply.github.com>
2024-04-25 18:44:26 +0900

    Title: 좀 더 어려운 수학 점수, Time: 229ms, Memory: 24MB, Status: Passed - CodeTree

--- 240425/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
+++ 240425/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -6,11 +6,7 @@ eng1 = int(arr1[1])
 math2 = int(arr2[0])
 eng2 = int(arr2[1])
 
-if math1 > math2:
-    print("A")
-elif math2 > math1:
-    print("B")
-elif eng1 > eng2:
+if math1 > math2 or (math1 == math2 and eng1 > eng2):
     print("A")
 else:
     print("B")
