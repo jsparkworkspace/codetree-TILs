b548aedbf96284a93c663a5ec4cba853ee5844c5
JunTR <150869047+jsparkworkspace@users.noreply.github.com>
2024-05-10 14:09:01 +0900

    Title: 진수 to 진수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240510/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        String n = sc.next();
+        int[] arr = new int[n.length()];
+
+        int num = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = n.charAt(i) - '0';
+            num = num * a + arr[i];
+        }
+
+        int[] result = new int[10];
+        int index = 0;
+        while (true) {
+            if (num < b) {
+                result[index++] = num;
+                break;
+            }
+
+            result[index++] = num % b;
+            num /= b;
+
+        }
+
+        for (int i = index - 1; i >= 0; i--) {
+            System.out.print(result[i]);
+        }
+
+        sc.close();
+    }
+}
